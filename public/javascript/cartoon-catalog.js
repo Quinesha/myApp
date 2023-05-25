73c14657167b9bde567c6ec3004db3bbe91fd3aa
Quinesha <qtiemations@gmail.com>
2023-05-25 11:50:39 +0200

    Refactoring

---
+++ public/javascript/cartoon-catalog.js
@@ -0,0 +1,43 @@
+const shows2 = [
+	{ id: 92685, name: "The owl house" },
+	{ id: 85349, name: "Amphibia" },
+	{ id: 95599, name: "Kipo" },
+	{ id: 40075, name: "Grafity falls" },
+	{ id: 61923, name: "Star vs the forces of evil" },
+	{ id: 37606, name: "The amazing world of Gumball" },
+	{ id: 15260, name: "Adventure Time" },
+	{ id: 61175, name: "Steven Universe" },
+	{ id: 2190, name: "South Park" },
+	{ id: 60625, name: "Rick and Morty" }
+]
+
+const apiKey2 = "144e7356ac0d3a69d8ea8bfa8b56c9f8"
+const baseUrl2 = "https://api.themoviedb.org/3/tv/"
+
+shows2.forEach(show => {
+	const url = `${baseUrl2}${show.id}?api_key=${apiKey2}`
+    
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			const showId = data.id
+			const showName = data.name
+			const overview = data.overview
+
+			const showData = document.getElementById("show-data")
+			const showElement = document.createElement("div")
+			showElement.innerHTML = `
+        <p>TV Show ID: ${showId}</p>
+        <p>Name: ${showName}</p>
+        <p class="results-p">Overview: ${overview}</p>
+        <img class="resultImages" src="/images/${showName.toLowerCase().replace(/\s/g, "-")}.jpg" alt="A titlecard image of the show ${showName}">
+        `
+			//All images used originate from the show's they are named after, and belong to the respective creators of these shows.
+
+			showData.appendChild(showElement)
+		})
+		.catch(error => {
+			console.log("Error occurred while fetching show data:", error)
+		})
+})
+
