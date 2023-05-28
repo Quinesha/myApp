0d5dca75d6293e5a2de96a49d6fd57f7a00abf03
Quinesha <qtiemations@gmail.com>
2023-05-29 01:24:20 +0200

    lots of stuff

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -1,33 +1,43 @@
 const shows = [
+	{ id: 92685, name: "The owl house" },
+	{ id: 85349, name: "Amphibia" },
+	{ id: 95599, name: "Kipo" },
+	{ id: 40075, name: "Grafity falls" },
+	{ id: 61923, name: "Star vs the forces of evil" },
+	{ id: 37606, name: "The amazing world of Gumball" },
 	{ id: 15260, name: "Adventure Time" },
 	{ id: 61175, name: "Steven Universe" },
+	{ id: 2190, name: "South Park" },
 	{ id: 60625, name: "Rick and Morty" }
 ]
-
+  
 const apiKey = "144e7356ac0d3a69d8ea8bfa8b56c9f8"
 const baseUrl = "https://api.themoviedb.org/3/tv/"
-
-shows.forEach(show => {
-	const url = `${baseUrl}${show.id}?api_key=${apiKey}`
-    
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			const showId = data.id
-			const showName = data.name
-			const overview = data.overview
-
-			const showData = document.getElementById("show-data")
-			const showElement = document.createElement("div")
-			showElement.innerHTML = `
-          <p>TV Show ID: ${showId}</p>
-          <p>Name: ${showName}</p>
-          <p class="results-p">Overview: ${overview}</p>
-          <img class="resultImages" src="/images/${showName.toLowerCase().replace(/\s/g, "-")}.jpg" alt="A titlecard image of the show ${showName}">
-        `
-			showData.appendChild(showElement)
-		})
-		.catch(error => {
-			console.log("Error occurred while fetching show data:", error)
-		})
-})
+  
+// Generate a random index within the range of the shows array length
+const randomIndex = Math.floor(Math.random() * shows.length)
+const randomShow = shows[randomIndex]
+  
+const url = `${baseUrl}${randomShow.id}?api_key=${apiKey}`
+  
+fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		const showId = data.id
+		const showName = data.name
+		const overview = data.overview
+  
+		const showData = document.getElementById("show-data")
+		const showElement = document.createElement("div")
+		showElement.innerHTML = `
+		<p>TV Show ID: ${showId}</p>
+		<p>Name: ${showName}</p>
+		<p class="results-p">Overview: ${overview}</p>
+		<img class="resultImages" src="/images/${showName.toLowerCase().replace(/\s/g, "-")}.jpg" alt="A titlecard image of the show ${showName}">
+	  `
+		showData.appendChild(showElement)
+	})
+	.catch(error => {
+		console.log("Error occurred while fetching show data:", error)
+	})
+  
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const { engine } = require("express-handlebars")
 const session = require("express-session")
 const bcrypt = require("bcryptjs")
 const mongoose = require("mongoose")
+const https = require("https")
+
+app.use(express.static("public"))
+
 
 require("dotenv").config()
-
 
 // The User schema defined
 const userSchema = new mongoose.Schema({
@@ -15,12 +18,23 @@ const userSchema = new mongoose.Schema({
 	age: Number,
 	username: String,
 	password: String,
+	savedShows: [{ name: String, overview: String }],
 })
 
 // Create the User model
 const User = mongoose.model("User", userSchema)
 
-app.engine("handlebars", engine({ defaultLayout: "main" }))
+app.engine(
+	"handlebars",
+	engine({
+		defaultLayout: "main",
+		// Disable prototype access check
+		runtimeOptions: {
+			allowProtoPropertiesByDefault: true,
+			allowProtoMethodsByDefault: true,
+		},
+	})
+)
 app.set("view engine", "handlebars")
 app.set("views", "views")
 
@@ -58,44 +72,90 @@ mongoose
 		})
 
 		app.get("/quiz", (req, res) => {
-			res.render("quiz")
+			res.render("quiz", { error: "" })
 		})
 
-		app.get("/quizQuestion2", (req, res) => {
-			res.render("quizQuestion2")
+		app.post("/quiz", (req, res) => {
+			// Process the quiz submission
+			// Here, you can add the logic to check the answers and calculate the score
+
+			// For simplicity, let's assume all answers are correct
+			const score = 5 // Total score
+			res.render("results", { score: score })
 		})
 
-		app.get("/quizQuestion3", (req, res) => {
-			res.render("quizQuestion3")
-		})
-
-		app.get("/quizQuestion4", (req, res) => {
-			res.render("quizQuestion4")
-		})
-
-		app.get("/quizQuestion5", (req, res) => {
-			res.render("quizQuestion5")
-		})
+		function getRandomShowId() {
+			// Replace this code with your own logic to retrieve a random show ID
+			const showIds = [92685, 85349, 95599, 40075, 61923, 37606, 15260, 61175, 2190, 60625] // Example show IDs
+			const randomIndex = Math.floor(Math.random() * showIds.length)
+			return showIds[randomIndex]
+		}
 
 		app.get("/results", (req, res) => {
-			res.render("results")
+			// Retrieve the randomly selected showId (Replace this with your own logic)
+			const showId = getRandomShowId()
+
+			// Construct the API endpoint URL using the showId
+			const apiEndpoint = `https://api.themoviedb.org/3/tv/${showId}?api_key=${process.env.API_KEY}`
+
+			// Send an HTTP request to the API endpoint
+			https.get(apiEndpoint, (apiResponse) => {
+				let data = ""
+
+				apiResponse.on("data", (chunk) => {
+					data += chunk
+				})
+
+				apiResponse.on("end", () => {
+					try {
+						const show = JSON.parse(data)
+						const showName = show.name
+						const showOverview = show.overview
+
+						// Send the show data back as the response
+						res.send({ show: show, showName: showName, showOverview: showOverview })
+					} catch (error) {
+						console.error("Error parsing API response:", error)
+						res.status(500).render("error", { error: "Failed to parse API response" })
+					}
+				})
+			}).on("error", (error) => {
+				console.error("Error making API request:", error)
+				res.status(500).render("error", { error: "Failed to fetch data from API" })
+			})
 		})
 
 		app.get("/shows", (req, res) => {
 			res.render("shows")
 		})
 
-		app.get("/savedShows", (req, res) => {
-			// Check if the user is logged in
-			if (req.session.loggedIn) {
-				// Render the savedShows view
-				const username = req.session.username
-				res.render("savedShows", { username: username })
-			} else {
-				// User is not logged in, redirect to the login page
-				res.redirect("/login")
+		app.get("/savedShows", async (req, res) => {
+			try {
+				// Check if the user is logged in
+				if (req.session.loggedIn) {
+					// Find the current user based on the session username
+					const foundUser = await User.findOne({ username: req.session.username })
+	
+					if (foundUser) {
+						// Retrieve the saved shows for the user
+						const savedShows = foundUser.savedShows
+	
+						// Render the savedShows view with the username and saved shows data
+						res.render("savedShows", { username: req.session.username, savedShows: savedShows })
+					} else {
+						// User not found, redirect to the login page
+						res.redirect("/login")
+					}
+				} else {
+					// User is not logged in, redirect to the login page
+					res.redirect("/login")
+				}
+			} catch (err) {
+				console.log(err)
+				res.render("savedShows", { username: req.session.username, savedShows: [] })
 			}
 		})
+	
 
 		app.get("/login", (req, res) => {
 			const errorMessage = req.session.error
@@ -116,21 +176,21 @@ mongoose
 		// Signup as a new user
 		app.post("/signup", (req, res) => {
 			const { username, password, confirmPassword, name, email, age } = req.body
-		
+
 			// Check if username is filled in
 			if (!username) {
 				req.session.error = "Username is required"
 				res.redirect("/signup") // Redirect to the signup page
 				return
 			}
-		
+
 			// Check if password and confirm password match
 			if (password !== confirmPassword) {
 				req.session.error = "Passwords do not match"
 				res.render("signUp", { error: req.session.error }) // Render the signup page with the error message
 				return
 			}
-		
+
 			// Hash the password
 			bcrypt.hash(password, 10, (err, hashedPassword) => {
 				if (err) {
@@ -139,7 +199,7 @@ mongoose
 					res.redirect("/signup") // Redirect to the signup page
 					return
 				}
-		
+
 				// Create a new user object with the filled-in information
 				const newUser = new User({
 					name: name,
@@ -148,7 +208,7 @@ mongoose
 					username: username,
 					password: hashedPassword,
 				})
-		
+
 				// Save the new user to the database
 				newUser
 					.save()
@@ -171,7 +231,6 @@ mongoose
 					})
 			})
 		})
-		
 
 		app.post("/login", (req, res) => {
 			const { username, password } = req.body
@@ -213,16 +272,126 @@ mongoose
 			res.redirect("/")
 		})
 
+		app.post("/addShow", async (req, res) => {
+			try {
+				// Check if the user is logged in
+				if (req.session.loggedIn) {
+					// Find the current user based on the session username
+					const foundUser = await User.findOne({ username: req.session.username })
+	
+					if (foundUser) {
+						// Retrieve the show details from your API
+						const showId = getRandomShowId() // Replace with your logic to get the show ID
+						const apiEndpoint = `https://api.themoviedb.org/3/tv/${showId}?api_key=${process.env.API_KEY}`
+	
+						// Make the API request to fetch the show data
+						https.get(apiEndpoint, async (apiRes) => {
+							let data = ""
+	
+							apiRes.on("data", (chunk) => {
+								data += chunk
+							})
+	
+							apiRes.on("end", async () => {
+								const show = JSON.parse(data)
+	
+								// Check if the show is already saved
+								const isShowSaved = foundUser.savedShows.some(
+									(savedShow) => savedShow.name === show.name && savedShow.overview === show.overview
+								)
+	
+								console.log("Received name:", show.name) // Log the received name
+	
+								if (isShowSaved) {
+									// Show is already saved, redirect back to savedShows
+									console.log("Show already saved:", show.name)
+									res.redirect("/savedShows")
+								} else {
+									// Create a show object with the name and overview from the API response
+									const showData = {
+										name: show.name,
+										overview: show.overview,
+									}
+	
+									// Add the show to the user's savedShows array
+									foundUser.savedShows.push(showData)
+	
+									// Save the user with the updated savedShows array
+									await foundUser.save()
+									console.log("Show saved:", show.name)
+									res.redirect("/savedShows")
+								}
+							})
+						})
+					} else {
+						// User not found, redirect to the login page
+						console.log("User not found")
+						res.redirect("/login")
+					}
+				} else {
+					// User is not logged in, redirect to the login page
+					console.log("User not logged in")
+					res.redirect("/login")
+				}
+			} catch (err) {
+				console.log(err)
+				res.redirect("/savedShows")
+			}
+		})
+
+		app.post("/removeShow/:showId", async (req, res) => {
+			try {
+				// Check if the user is logged in
+				if (req.session.loggedIn) {
+					// Find the current user based on the session username
+					const foundUser = await User.findOne({ username: req.session.username })
+	
+					if (foundUser) {
+						const showId = req.params.showId // Get the show ID from the request params
+	
+						// Find the index of the show with the given ID in the savedShows array
+						const showIndex = foundUser.savedShows.findIndex((savedShow) => savedShow._id == showId)
+	
+						if (showIndex !== -1) {
+							// Remove the show from the user's savedShows array at the specified index
+							foundUser.savedShows.splice(showIndex, 1)
+	
+							// Save the user with the updated savedShows array
+							await foundUser.save()
+							console.log("Show removed with ID:", showId)
+						} else {
+							console.log("Show not found with ID:", showId)
+						}
+					} else {
+						// User not found, redirect to the login page
+						console.log("User not found")
+						res.redirect("/login")
+						return
+					}
+				} else {
+					// User is not logged in, redirect to the login page
+					console.log("User not logged in")
+					res.redirect("/login")
+					return
+				}
+	
+				res.redirect("/savedShows")
+			} catch (err) {
+				console.log(err)
+				res.redirect("/savedShows")
+			}
+		})
+	
+
 		app.use((req, res) => {
 			res.status(404).render("404", { imagePath: "/images/404.png" })
 		})
 
-		app.listen(8080, () => {
-			console.log("Server is running on port 8080")
+		const PORT = process.env.PORT || 8080
+		app.listen(PORT, () => {
+			console.log(`Server listening on port ${PORT}`)
 		})
 	})
 	.catch((error) => {
 		console.error("Error connecting to the database:", error)
 	})
-
-	
