20e86c725d405aa2605c5131aa945af26241bf30
Quinesha <qtiemations@gmail.com>
2023-06-08 11:24:36 +0200

    refactoring

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,39 +1,39 @@
 // Retrieve form data from localStorage if available
-const storedFormData = localStorage.getItem('signupFormData')
+const storedFormData = localStorage.getItem("signupFormData")
 if (storedFormData) {
-    const formData = JSON.parse(storedFormData)
-    populateForm(formData)
+	const formData = JSON.parse(storedFormData)
+	populateForm(formData)
 }
 
 // Add submit event listener to the form
-const form = document.getElementById('signupForm')
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent form submission
+const form = document.getElementById("signupForm")
+form.addEventListener("submit", (e) => {
+	e.preventDefault() // Prevent form submission
 
-    // Get form data
-    const formData = getFormData()
+	// Get form data
+	const formData = getFormData()
 
-    // Store form data in localStorage
-    localStorage.setItem('signupFormData', JSON.stringify(formData))
+	// Store form data in localStorage
+	localStorage.setItem("signupFormData", JSON.stringify(formData))
 
-    // Submit the form
-    form.submit()
-});
+	// Submit the form
+	form.submit()
+})
 
 // Function to get form data as an object
 function getFormData() {
-    const formData = {}
-    const inputs = document.querySelectorAll('#signupForm input')
-    inputs.forEach((input) => {
-        formData[input.name] = input.value
-    })
-    return formData
+	const formData = {}
+	const inputs = document.querySelectorAll("#signupForm input")
+	inputs.forEach((input) => {
+		formData[input.name] = input.value
+	})
+	return formData
 }
 
 // Function to populate form fields with data
 function populateForm(formData) {
-    const inputs = document.querySelectorAll('#signupForm input')
-    inputs.forEach((input) => {
-        input.value = formData[input.name] || ''
-    })
+	const inputs = document.querySelectorAll("#signupForm input")
+	inputs.forEach((input) => {
+		input.value = formData[input.name] || ""
+	})
 }
