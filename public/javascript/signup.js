acc9f33b535dadf7c908a32d2eee389ccc43bcc5
Quinesha <qtiemations@gmail.com>
2023-06-12 17:33:15 +0200

    ES5 to ES6

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,27 +1,35 @@
+// Populate form fields with data
+const populateForm = (formData) => {
+	const inputs = document.querySelectorAll("#signupForm input")
+	inputs.forEach((input) => {
+		input.value = formData[input.name] || ""
+	})
+}
+  
 // Retrieve form data from localStorage if available
 const storedFormData = localStorage.getItem("signupFormData")
 if (storedFormData) {
 	const formData = JSON.parse(storedFormData)
 	populateForm(formData)
 }
-
+  
 // Add submit event listener to the form
 const form = document.getElementById("signupForm")
 form.addEventListener("submit", (e) => {
 	e.preventDefault() // Prevent form submission
-
+  
 	// Get form data
 	const formData = getFormData()
-
+  
 	// Store form data in localStorage
 	localStorage.setItem("signupFormData", JSON.stringify(formData))
-
+  
 	// Submit the form
 	form.submit()
 })
-
-// Function to get form data as an object
-function getFormData() {
+  
+// Get form data as an object
+const getFormData = () => {
 	const formData = {}
 	const inputs = document.querySelectorAll("#signupForm input")
 	inputs.forEach((input) => {
@@ -29,11 +37,4 @@ function getFormData() {
 	})
 	return formData
 }
-
-// Function to populate form fields with data
-function populateForm(formData) {
-	const inputs = document.querySelectorAll("#signupForm input")
-	inputs.forEach((input) => {
-		input.value = formData[input.name] || ""
-	})
-}
+  
